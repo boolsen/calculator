37fefd4e5a33cffe812acd97c907963968e816ac
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-01-30 22:56:11 +0100

    Added events for buttonpress, finished css setup

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
+let firstNum;
+let secondNum;
+let operation;
+let result;
+
+const wrapper = document.querySelector('.container');
+const display = document.getElementById('calc-display');
+
+wrapper.addEventListener('click', (event) => {
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+
+  buttonManager(event.target)
+
+})
+
 const add = function(num1, num2) {
     return num1 + num2;
 }
@@ -15,4 +33,54 @@ const divide = function (num1, num2) {
         return 'ERROR';
     }
     return num1 / num2;
+}
+
+const calculate = function (num1, num2, operator) {
+    let res;
+    switch (operator) {
+        case "+":
+            res = add(num1, num2);
+            break;
+
+        case "-":
+            res = subtract(num1,num2);            
+            break;
+
+        case "*":
+            res = multiply(num1, num2);
+            break;
+
+        case "/":
+            res = divide(num1, num2);
+            break;
+    
+        default:
+            break;
+    }
+}
+
+const buttonManager = function (target) {
+    const isActive = button.classList.contains("active");
+    let targetClasses = target.classList;
+
+    if (targetClasses.contains('number')) {
+        if (display.textContent === '0') {
+            display.textContent = target.textContent;
+        }
+        display.textContent += target.textContent; 
+    }
+    else if (targetClasses.contains('operation')) {
+        if (operation != null) {
+            calculate(target);
+        }
+        operation = target.textContent;
+    }
+    else if (targetClasses.contains('clear')) {
+        display.textContent = '';
+        operation = null;
+        result = null;
+    }
+    else if (targetClasses.contains('cakculate')) {
+        calculate(target);
+    }
 }
